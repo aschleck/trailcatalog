645dc00fecc056acbfd236fa5ecac82c1401b11a
April Schleck <april@exclusivelyducks.com>
2022-02-13 00:05:27 -0800

    Fetching data

--- java/org/trailcatalog/Frontend.kt
+++ java/org/trailcatalog/Frontend.kt
@@ -1,28 +1,60 @@
 package org.trailcatalog
 
 import com.google.common.geometry.S2CellId
+import com.google.common.geometry.S2LatLngRect
+import com.google.common.primitives.UnsignedLongs
 import com.google.devtools.build.runfiles.Runfiles
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import io.javalin.Javalin
+import io.javalin.core.util.Headers
 import io.javalin.http.Context
+import java.io.ByteArrayOutputStream
+import java.io.DataOutputStream
 import java.sql.DriverManager
 
-val connectionSource = HikariDataSource(HikariConfig())
+val connectionSource = HikariDataSource(HikariConfig().apply {
+  jdbcUrl =
+    "jdbc:postgresql://10.110.231.203:5432/trailcatalog?currentSchema=migration_1_create_geometry"
+  username = "postgres"
+  password = "postgres"
+})
 
 fun main(args: Array<String>) {
-  val connection =
-      DriverManager.getConnection(
-          "jdbc:postgresql://10.110.231.203:5432/trailcatalog",
-          "postgres",
-          "postgres")
-
-  val runfiles = Runfiles.create()
   val app = Javalin.create {}.start(7070)
   app.get("/api/fetch_cell/{token}", ::fetch_cell)
 }
 
+data class WireWay(val id: Long, val type: Int, val vertices: ByteArray)
+
 fun fetch_cell(ctx: Context) {
-  val cell = S2CellId.fromToken(ctx.pathParam("token"));
-  ctx.result("Cell: " + cell)
+  ctx.contentType("application/octet-stream")
+  val cell = S2CellId.fromToken(ctx.pathParam("token"))
+  val ways = ArrayList<WireWay>()
+  connectionSource.connection.use {
+    val query = it.prepareStatement("SELECT id, type, points_bytes FROM ways WHERE cell = ?").apply {
+      setLong(1, cell.id())
+    }
+    val results = query.executeQuery()
+    while (results.next()) {
+      ways.add(WireWay(
+          id = results.getLong(1),
+          type = results.getInt(2),
+          vertices = results.getBytes(3),
+      ))
+    }
+  }
+
+  val bytes = ByteArrayOutputStream()
+  val output = DataOutputStream(bytes)
+  output.writeInt(ways.size)
+  for (way in ways) {
+    output.writeLong(way.id)
+    output.writeInt(way.type)
+    output.writeInt(way.vertices.size)
+  }
+  for (way in ways) {
+    output.write(way.vertices)
+  }
+  ctx.result(bytes.toByteArray())
 }
--- java/org/trailcatalog/client/app.ts
+++ java/org/trailcatalog/client/app.ts
@@ -3,7 +3,7 @@ import { S1Angle, S2LatLng, S2LatLngRect, SimpleS2 } from 'java/org/trailcatalog
 
 console.log('hello!');
 
-const viewport = S2LatLngRect.fromPoint(S2LatLng.fromDegrees(37.424862, -122.154853)).expandedByDistance(S1Angle.degrees(0.01));
+const viewport = S2LatLngRect.fromPoint(S2LatLng.fromDegrees(47.644209, -122.139532)).expandedByDistance(S1Angle.degrees(0.01));
 
 const cells = SimpleS2.cover(viewport);
 console.log(cells);
