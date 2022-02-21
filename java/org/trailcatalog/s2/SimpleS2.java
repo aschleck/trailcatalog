7c13156ba156fce6e6ea65e2a1526f1c0a760d0f
April Schleck <april@exclusivelyducks.com>
2022-02-20 23:11:45 -0800

    Made generating the covering faster

--- java/org/trailcatalog/s2/SimpleS2.java
+++ java/org/trailcatalog/s2/SimpleS2.java
@@ -2,6 +2,7 @@ package org.trailcatalog.s2;
 
 import com.google.common.geometry.S1Angle;
 import com.google.common.geometry.S2CellId;
+import com.google.common.geometry.S2CellUnion;
 import com.google.common.geometry.S2LatLng;
 import com.google.common.geometry.S2LatLngRect;
 import com.google.common.geometry.S2RegionCoverer;
@@ -19,23 +20,15 @@ public final class SimpleS2 {
   @JsMethod
   public static ArrayList<S2CellId> cover(S2LatLngRect viewport) {
     ArrayList<S2CellId> cells = new ArrayList<>();
+    S2CellUnion union = new S2CellUnion();
     ArrayList<S2CellId> atLevel = new ArrayList<>();
-    ArrayList<S2CellId> expanded = new ArrayList<>();
-    Set<S2CellId> withNeighborsAtLevel = new HashSet<>();
     for (int level = HIGHEST_INDEX_LEVEL; level >= 0; --level) {
       S2RegionCoverer coverer =
           S2RegionCoverer.builder().setMaxCells(1000).setMinLevel(level).setMaxLevel(level).build();
-      coverer.getCovering(viewport, atLevel);
-
-      withNeighborsAtLevel.addAll(atLevel);
-      for (S2CellId cell : atLevel) {
-        expanded.clear();
-        cell.getAllNeighbors(cell.level(), expanded);
-        withNeighborsAtLevel.addAll(expanded);
-      }
-
-      cells.addAll(withNeighborsAtLevel);
-      withNeighborsAtLevel.clear();
+      coverer.getCovering(viewport, union);
+      union.expand(level);
+      union.denormalize(level, /* levelMod= */ 1, atLevel);
+      cells.addAll(atLevel);
     }
     return cells;
   }
