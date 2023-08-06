06443ad4a0f80abd5171bf996239e6e2f6bec47d
April Schleck <april@exclusivelyducks.com>
2023-08-06 15:01:48 -0700

    Use the tmpdir since the default is too small

--- production/containers/build.sh
+++ production/containers/build.sh
@@ -7,6 +7,6 @@ bazelisk build '//production/containers:frontend_pkg.tar'
 cp --no-preserve=mode \
     ../../bazel-bin/production/containers/frontend_pkg.tar \
     .
-buildah bud -f Containerfile --layers --tag localhost/bazel/production/containers/frontend:latest
+TMPDIR=/tmp buildah bud -f Containerfile --layers --tag localhost/bazel/production/containers/frontend:latest
 gcloud auth print-access-token | podman login -u oauth2accesstoken --password-stdin us-west1-docker.pkg.dev
 podman push localhost/bazel/production/containers/frontend:latest us-west1-docker.pkg.dev/trailcatalog/containers/frontend:latest
