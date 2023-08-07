fece4eb68867d45ee620de5b91c54ac5a5ca28e1
April Schleck <april@exclusivelyducks.com>
2023-08-06 22:07:27 -0700

    Move progress stuff into Tailwind

--- java/lat/trails/client/progress.tsx
+++ java/lat/trails/client/progress.tsx
@@ -2,14 +2,47 @@ import * as corgi from 'js/corgi';
 
 export function IndeterminantCircular() {
   return <>
-    <div className="indeterminant-circular h-8 relative text-3xl w-8"></div>
+    <div
+        className="
+            h-8
+            relative
+            text-3xl
+            w-8
+            before:absolute
+            before:animate-spin
+            before:border-[0.1em]
+            before:border-white-opaque-250
+            before:border-b-transparent
+            before:content-['']
+            before:h-[1em]
+            before:rounded-full
+            before:w-[1em]
+        "
+    >
+    </div>
   </>;
 }
 
 export function IndeterminantLinear() {
   return <>
     <div
-        className="indeterminant-linear absolute h-1 left-0 right-0 select-none top-0 touch-none"
+        className="
+            absolute
+            h-1
+            left-0
+            right-0
+            select-none
+            top-0
+            touch-none
+            before:animate-slide
+            before:bg-tc-black-800
+            before:bottom-0
+            before:content-['']
+            before:left-0
+            before:absolute
+            before:top-0
+            before:w-1/3
+        "
     >
     </div>
   </>;
--- java/lat/trails/client/tailwind.theme.mjs
+++ java/lat/trails/client/tailwind.theme.mjs
@@ -8,10 +8,20 @@ export default {
       },
       'white-opaque': {
         160: '#ffffffa0',
+        250: '#fffffffa',
       },
     },
     fontFamily: {
       'sans': ['"Source Sans 3"', ...defaultTheme.fontFamily.sans],
+    },
+    animation: {
+      slide: 'slide 1.75s ease-in-out infinite',
+    },
+    keyframes: {
+      slide: {
+        '0%': {left: '-33%'},
+        '100%': {left: '100%'},
+      },
     },
   },
 };
