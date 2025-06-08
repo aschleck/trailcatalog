5786389f030c2fc648c025a359c4bf0de478270d
April Schleck <april@exclusivelyducks.com>
2025-06-08 09:25:57 -0700

    Make the skybox shader slightly less broken since I accidentally already launched this

--- js/map/rendering/skybox_program.ts
+++ js/map/rendering/skybox_program.ts
@@ -1,4 +1,4 @@
-import { checkExists } from 'external/dev_april_corgi~/js/common/asserts';
+import { checkExists } from 'external/dev_april_corgi+/js/common/asserts';
 
 import { RgbaU32, Vec2 } from '../common/types';
 
@@ -113,33 +113,41 @@ function createSkyboxProgram(gl: WebGL2RenderingContext): SkyboxProgramData {
       const float FOV = PI / 4.;
 
       void main() {
-        gl_Position = vec4(position, z + 1., 1.) + 0. * sphericalMvp * cameraCenter * halfWorldSize;
+        gl_Position = vec4(position, z + 1., 1.) + 0. * sphericalMvp * cameraCenter * halfWorldSize * flattenFactor + vec4(0.000001 * inverseHalfViewportSize, 0., 0.);
 
-        vec2 mercator = vec2(0);
+        // TODO(april): this doesn't work. There's another approach in ed07d813 that seems righter
+        // but also doesn't work. Alas.
 
-        float viewportRadiusWorldUnitsAtLat =
-          PI * cos(0.) / halfWorldSize / inverseHalfViewportSize.y;
-        float distanceCameraToGlobeSurface = viewportRadiusWorldUnitsAtLat / tan(FOV / 2.);
-        float scale = 1. + distanceCameraToGlobeSurface;
-
-        float angleOriginGlobeEdge = asin(1. / scale);
-        float angleCenterGlobeEdge = PI / 2. - angleOriginGlobeEdge;
-
-        float sinLat = tanh(0. * PI);
+        // Figure out where the edge of the globe on X is
+        vec2 cameraMerged = vec2(cameraCenter.x + cameraCenter.y + 0.5, 0);
+        float sinLat = tanh(cameraMerged.y * PI);
         float lat = asin(sinLat);
         float cosLat = cos(lat);
-        float lng = (cameraCenter.x + cameraCenter.y) * PI + angleCenterGlobeEdge;
-        vec4 p = sphericalMvp * vec4(
+        float lng = cameraMerged.x * PI;
+        vec4 sphericalRight = sphericalMvp * vec4(
             cosLat * cos(lng), // x
             sinLat,            // y
             cosLat * sin(lng), // z
             1.0                // w
         );
-        vec2 trueRadius = p.xy / p.w;
 
-        // Try to figure out the screen coordinate of position in units of trueRadius
-        vec2 spherical = position / trueRadius;
-        fragPosition = mix(spherical, mercator, flattenFactor);
+        // Figure out where the edge of the globe on Y is
+        cameraMerged =
+          vec2(cameraCenter.x + cameraCenter.y, cameraCenter.z + cameraCenter.w + 1.);
+        sinLat = tanh(cameraMerged.y * PI);
+        lat = asin(sinLat);
+        cosLat = cos(lat);
+        lng = cameraMerged.x * PI;
+        vec4 sphericalTop = sphericalMvp * vec4(
+            cosLat * cos(lng), // x
+            sinLat,            // y
+            cosLat * sin(lng), // z
+            1.0                // w
+        );
+
+        vec2 spherical =
+          position / vec2(sphericalRight.x / sphericalRight.w, sphericalTop.y / sphericalTop.w);
+        fragPosition = spherical;
       }
     `;
   const fs = `#version 300 es
@@ -148,7 +156,9 @@ function createSkyboxProgram(gl: WebGL2RenderingContext): SkyboxProgramData {
       out mediump vec4 fragColor;
 
       void main() {
-        fragColor = length(fragPosition) > 1. ? vec4(fragPosition, 0., 1.) : vec4(0);
+        // TODO(april): delete this debugging pattern
+        // fragColor = length(fragPosition) > 1. ? vec4(fragPosition, 0., 1.) : vec4(0);
+        fragColor = length(fragPosition) > 1. ? vec4(0.05, 0.05, 0.05, 1.) : vec4(0);
       }
   `;
 
@@ -156,7 +166,7 @@ function createSkyboxProgram(gl: WebGL2RenderingContext): SkyboxProgramData {
   gl.shaderSource(vertexId, vs);
   gl.compileShader(vertexId);
   if (!gl.getShaderParameter(vertexId, gl.COMPILE_STATUS)) {
-    throw new Error(`Unable to compile sdf vertex shader: ${gl.getShaderInfoLog(vertexId)}`);
+    throw new Error(`Unable to compile skybox vertex shader: ${gl.getShaderInfoLog(vertexId)}`);
   }
   gl.attachShader(programId, vertexId);
 
@@ -164,13 +174,13 @@ function createSkyboxProgram(gl: WebGL2RenderingContext): SkyboxProgramData {
   gl.shaderSource(fragmentId, fs);
   gl.compileShader(fragmentId);
   if (!gl.getShaderParameter(fragmentId, gl.COMPILE_STATUS)) {
-    throw new Error(`Unable to compile sdf fragment shader: ${gl.getShaderInfoLog(fragmentId)}`);
+    throw new Error(`Unable to compile skybox fragment shader: ${gl.getShaderInfoLog(fragmentId)}`);
   }
   gl.attachShader(programId, fragmentId);
 
   gl.linkProgram(programId);
   if (!gl.getProgramParameter(programId, gl.LINK_STATUS)) {
-    throw new Error(`Unable to link sdf program: ${gl.getProgramInfoLog(programId)}`);
+    throw new Error(`Unable to link skybox program: ${gl.getProgramInfoLog(programId)}`);
   }
 
   return {
